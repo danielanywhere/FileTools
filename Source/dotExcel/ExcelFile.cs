d8b90831c663c3f843e0cb291aa4171d2f7b56c9
Daniel Patterson <danielanywhere@users.noreply.github.com>
2026-02-16 08:22:21 -0600

    Copyright attribution update

--- Source/dotExcel/ExcelFile.cs
+++ Source/dotExcel/ExcelFile.cs
@@ -1,30 +1,34 @@
 /*
- * Copyright (c). 2016-2026 Daniel Patterson, MCSD (danielanywhere).
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- * 
- */
+* Copyright (c). 2020-2026 Daniel Patterson, MCSD (danielanywhere).
+* 
+* Contributions: Copyright (c). 2023 Jason Ge
+* https://jason-ge.medium.com/create-excel-using-openxml-in-net-6-3b601ddf48f7
+* 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+* 
+*/
 
-using DocumentFormat.OpenXml;
-using DocumentFormat.OpenXml.Packaging;
-using DocumentFormat.OpenXml.Spreadsheet;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
 
 namespace dotExcel
 {
